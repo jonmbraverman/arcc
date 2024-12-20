2d3863ef6bfbbd15bbd8909228b43b93055e7dca
jonathanbraverman <jonathan.braverman@ctstate.edu>
2024-12-20 22:00:53 +0000

    t

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main(int argc, char **argv) {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *ip = "192.168.1.146";
+    char *hello = "Hello, Server!!!";
+    char buffer[1024] = {0};
+
+    if(argc == 2)
+    {
+        ip = argv[1];
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent to %s\n",ip);
+    close(sock);
+    return 0;
+}
